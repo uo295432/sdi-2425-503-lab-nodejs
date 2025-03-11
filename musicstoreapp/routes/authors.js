4ce77fba845bc8cf61bad89e5ff35ca55f9fcd62
uo295432 <uo295432@uniovi.es>
2025-03-11 10:00:42 +0100

    SDI-2425-503-sesión8-ejercicio-complementario1

---
+++ musicstoreapp/routes/authors.js
@@ -0,0 +1,54 @@
+module.exports = function (app) {
+    app.get('/authors/add', function (req, res) {
+        res.render("authors/add.twig");
+    });
+
+    app.post('/authors/add',function(req,res){
+        let response="Autor agregado: ";
+        if(req.body.name!=null && typeof(req.body.name)!="undefined" && req.body.name.trim() !== ""){
+            response += ('Nombre: ' + req.body.name + '<br>');
+        }else{
+            response += "Nombre no enviado en la petición" + '<br>';
+        }
+        if(req.body.group!=null && typeof(req.body.group)!="undefined" && req.body.group.trim() !== ""){
+            response += ('Grupo: ' + req.body.group + '<br>');
+        }else{
+            response += "Grupo no enviado en la petición" + '<br>';
+        }
+        if(req.body.rol!=null && typeof(req.body.rol)!="undefined" && req.body.rol.trim() !== ""){
+            response += ('Rol: ' + req.body.rol);
+        }else{
+            response += "Rol no enviado en la petición" + '<br>';
+        }
+        res.send(response);
+    });
+
+    app.get('/authors', function (req, res){
+        let authors=[{
+            "name":"Kurt",
+            "group":"Nirvana",
+            "rol":"Cantante"
+        },{
+            "name":"Eazy-E",
+            "group":"NWA",
+            "rol":"Rapero"
+        },{
+            "name":"Dr Dre",
+            "group":"NWA",
+            "rol":"DJ"
+        }];
+
+        let response={
+            seller:'Autores',
+            authors:authors
+        };
+        res.render("authors/authors.twig",response);
+    });
+
+    app.get('/aut*', function (req, res) {
+        res.redirect('/authors/');
+    });
+    app.get('/authors/*', function (req, res) {
+        res.redirect('/authors/');
+    });
+};
