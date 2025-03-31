4a55cea50c187cde55914c5d34cf5d0f276115e3
uo295432 <uo295432@uniovi.es>
2025-03-31 16:47:31 +0200

    SDI-2425-503-11.3-SW Cliente JQuery + AJAX.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -9,6 +9,15 @@ let jwt = require('jsonwebtoken');
 app.set('jwt', jwt);
 let crypto = require('crypto');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, token");
+// Debemos especificar todas las headers que se aceptan. Content-Type , token
+  next();
+});
+
 let expressSession = require('express-session');
 app.use(expressSession({
   secret: 'abcdefg',
